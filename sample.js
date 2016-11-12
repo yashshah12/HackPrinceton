50aa41972c5ad304571d600b9eebfc4d453abfb7
Lucy Low <low.lucyy@gmail.com>
2016-11-12 17:14:51 -0500

    speech-to-text
    need to write XML(?) and GET(?)

--- sample.js
+++ sample.js
@@ -142,7 +142,127 @@
    }
 
    function getTextOfSpeech(audiofile){
+    var fs = require('fs'); // file system node.js 
+    var util = require('util'); // util module 
+    var request = require('request'); 
+
+    var clientId = 'test-meeting'; 
+    var clientKey = '8df19e9ec19c43d2a63444b3b38e0b9e'; // back-up key '20db5601a2414d2cbe5701e16a946fc0'
+
+    var str = 'tester string about stuff here'
+
+    getAccessToken(clientId, clientSecret, function(err, accessToken)) {
+
+      if(err) return console.log(err); 
+
+      console.log('Access token is:' + accessToken)
+
+      speechToText('test.wav', accessToken, function(err, res)) {
+        if(err) return console.log(err); 
+        console.log('Confidence' + res.results[0].confidence + 'for:' + res.results[0].lexical );
+      };
+    };
+   } // end getTextOfSpeech(audiofile)
+
+   // helper functions 
+
+   function getAccessToken(clientId, clientSecret, callback) {
+
+    //POST request
+    request.post({
+      url: 'https://oxford-speech.cloudapp.net/token/issueToken', 
+      form: {
+          'grant_type': 'client_credentials',
+          'client_id': encodeURIComponent(clientId),
+          'client_key': encodeURIComponent(clientKey),
+          'scope': 'https://speech.platform.bing.com'    
+      }
+
+    }, 
+    function(err, resp, body){
+      if(err) return callback(err);
+      try {
+        var accesstoken = JSON.parse(body).access_token; 
+        if(accessToken){
+          callback(null, accessToken);
+        } else {
+          callback(body);
+        }
+      } catch(e) {
+        callback(e);
+      }
+
+    });
+   }
+
+
+   function speechToText(audiofile, accessToken, callback){
+
+    fs.readFile(audiofile,function(err,waveData){
+
+      if(err) return callback(err); 
+
+      request.post({
+        url: 'https://speech.platform.bing.com/recognize/query',
+        qs: {
+          //'scenarios': 'ulm',
+          //'appid': 'D4D52672-91D7-4C74-8AD8-42B1D98141A5', // This magic value is required
+          //'locale': 'en-US',
+          //'device.os': 'wp7',
+          //'version': '3.0',
+          //'format': 'json',
+          //'requestid': '1d4b6030-9099-11e0-91e4-0800200c9a66', // can be anything
+          //'instanceid': '1d4b6030-9099-11e0-91e4-0800200c9a66' // can be anything
+        },
+
+        body: waveData, 
+        headers: {
+          'Authorization': 'Bearer ' + accessToken,
+          'Content-Type': 'audio/wav; samplerate=16000',
+          'Content-Length' : waveData.length
+        }
+
+      }, function(err,resp,body){
+        if(err) return callback(err); 
+
+        try {
+          callback(null, JSON.parse(body)); 
+        } catch(e) {
+          callback(e); 
+        }
+
+      });
+    });
 
    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
